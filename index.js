79ef0b0c46f4d5640b3e5e1ecedf5d65e2206e51
ramanapitla760-code <ramanapitla760@gmail.com>
2026-02-09 14:35:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// Hide intro after 2.5 seconds
+setTimeout(() => {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("main").style.display = "block";
+}, 2500);
